3557f1876753e68483f1c47aa74d6abe75d13cfc
orlovskiy <odi991@gmail.com>
2016-04-22 21:49:39 +0300

    hope so much

--- 4.range_test/index.php
+++ 4.range_test/index.php
@@ -5,33 +5,13 @@
 	<title>test</title>
 	<link rel="stylesheet" href="../style/rokich.css">
 	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/range_test.js"></script>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	<script src="../js/jquery-ui.min.js"></script>
 	<script src="../js/rokich.js"></script>
 	<link rel="stylesheet" href="../style/range_test.css">
 </head>
 <body>
-<script>
-	var gradations = {1:'нет',2:'не совсем',3:'нечто среднее',4:'почти',5:'абсолютно точно'}
-  $(function() {
-  	$('.question .slider-container').each(function(){
-  		var amount = $(this).find('#amount')
-	    $(this).find('#slider').slider({
-	      value:1,
-	      min: 1,
-	      max: 5,
-	      step: 1,
-	      slide: function( event, ui ) {
-	      amount.val(gradations[ui.value] );
-	      }
-	    });
-	});
-    	$('.question .slider-container').each(function(){
-  		var amount = $(this).find('#amount').val(gradations[$( "#slider" ).slider( "value" )] )
-  	});
-  });
-  </script>
-
 <div class="id" style="display: none"><input type="text" class="anon_id"  value="<?=$_GET['id']?>"></div>
 
 
@@ -66,6 +46,9 @@
 		<div id="slider"></div>
 		<input type="text" id="amount" readonly style="border:0;">
 	</div>
+</div>
+<div class="result_button">
+	<input class="show_result" type="button" value="Показать результат">
 </div>
 </body>
 </html>
